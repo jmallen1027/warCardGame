be0f243c4715fc3f3ecfa5beceaa04b401c0ff82
jmallen1027 <jmallen1027>
2022-06-27 13:31:06 -0400

    Add existing project files to Git

--- app.js
+++ app.js
@@ -1,36 +1,52 @@
-const shuffleBtn = document.querySelector('#shuffle')
-const warBtn = document.querySelector('#war')
-const player1Score = document.querySelector('.score-p1')
-const player2Score = document.querySelector('.score-p2')
+import { result, war, warCards, playerCardValue, playerSuite } from './game.js';
+import {deal, player1, player2} from './cards.js';
+
+const shuffleBtn = document.querySelector('#shuffle');
+const warBtn = document.querySelector('#p2war');
+const player1Deck = document.querySelector('#p1-deck')
+const player2Deck = document.querySelector('#p2-deck')
+const player1Score = document.querySelector('#score-p1');
+const player2Score = document.querySelector('#score-p2');
 const player1Styles = document.querySelectorAll('.player1-hand');
 const player2Styles = document.querySelectorAll('.player2-hand');
 
-import { result, war, warCards, playerCardValue, playerSuite } from './game.js'
-import {deal, player1, player2} from './cards.js'
+const visibleDocs = [warBtn, player1Deck, player2Deck]
 
-let p1Deck = player1.allCards.length
-let p2Deck = player2.allCards.length
+
+
+let p1Deck = player1.allCards.length;
+let p2Deck = player2.allCards.length;
 
 
 shuffleBtn.addEventListener('click', function(){
     
     deal()
-    
-    shuffleBtn.style.visibility =`hidden`;
-    warBtn.style.visibility =  'visible'
+    shuffleBtn.style.visibility = 'hidden'
+    visibleDocs.forEach(element => {
+        element.style.visibility = 'visible';
+    });
 
-    console.log(player1.allCards)
-    console.log(player2.allCards)
+    console.log(player1.allCards);
+    console.log(player2.allCards);
 
  })
 
 warBtn.addEventListener('click', function(){
 
+    for(let i = 0; i < 3; i++){
+        let q = i + 1
+        player1Styles[q].style.visibility =`hidden`;
+        player2Styles[q].style.visibility =`hidden`;
+    }
+    document.querySelector('#player1-score').innerHTML = player1.allCards.length
+    document.querySelector('#player2-score').innerHTML = player2.allCards.length
+    
     let playerOneCard = playerCardValue(player1.allCards[0]); //Sets the value of Player One's Card
     let playerTwoCard = playerCardValue(player2.allCards[0]); //Sets the value of Player Two's Card
     let playerOneSuite = playerSuite(player1.allCards[0]); //Sets Suite of Player 1's Card for Image. 
     let playerTwoSuite = playerSuite(player2.allCards[0]);
-
+    
+    player1Styles[0].style.visibility = 'visible'
     player1Styles[0].style.backgroundImage=`url(./cards/${playerOneSuite}/${playerOneCard}.png)`; //Sets the Image based on the Document Element of the Card. 
     player2Styles[0].style.backgroundImage=`url(./cards/${playerTwoSuite}/${playerTwoCard}.png)`;
 
